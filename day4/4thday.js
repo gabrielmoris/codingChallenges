989d8eec00065a711a605c6c17dbe8773e4cf23e
Gabriel C Moris <gabrieltrompeta@gmail.com>
2021-11-04 15:33:30 +0100

    finally finished the exercises

--- day4/4thday.js
+++ day4/4thday.js
@@ -1,10 +1,29 @@
 //EXERCISE 1
+function each(obArr, func){
+    if(Array.isArray(obArr)){
+        obArr.forEach(func);
+    } else if(typeof obArr === "object"){
+        for (var p in obArr) {
+        func(obArr[p], p)
+        }
+    } else {
+        return "ERROR"
+    }
+}
 
 
 
 
+console.log(each({
+       a: 1,
+       b: 2
+   }, function(val, name) {
+       console.log('The value of ' + name + ' is ' + val);
+   })); // logs 'the value of a is 1' and 'the value of b is 2'
 
-
+ console.log(each(['a', 'b'], function(val, idx) {
+       console.log('The value of item ' + idx + ' is ' + val);
+   })); // logs 'the value of item 0 is a' and 'the value of item 1 is b'
 
 //EXERCISE 2
 
