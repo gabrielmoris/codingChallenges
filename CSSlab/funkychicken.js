255599b624ae52675290826c592f4717e7d399b9
Gabriel C Moris <gabrieltrompeta@gmail.com>
2021-11-05 13:52:16 +0100

    it was AWESOME

---
+++ CSSlab/funkychicken.js
@@ -0,0 +1,23 @@
+var x;
+var xx;
+
+x= 156;
+
+function timesTwo(number) {
+    return number * 2;
+};
+
+xx = timesTwo(x);
+
+var numbers;
+
+numbers = [x, xx];
+
+for (var i=0; i<numbers.length; i++){
+    console.log(numbers[i])
+};
+
+numbers = {};
+
+y.numbers= xx;
+
