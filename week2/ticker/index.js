f5e4c50dba0b721008e6522c30e787e701a15321
Gabriel C Moris <gabrieltrompeta@gmail.com>
2021-11-10 17:47:18 +0100

    added footer in the ticker website

--- week2/ticker/index.js
+++ week2/ticker/index.js
@@ -27,3 +27,45 @@
         });
     }
 })();
+
+
+(function () {
+    var container = document.getElementsByClassName("footerline")[0];
+
+    var links = container.getElementsByTagName("A");
+
+    var left = container.offsetLeft + container.offsetWidth;
+
+    var myReqId;
+
+    function moveHeadlines() {
+        left--;
+
+        myReqId = requestAnimationFrame(moveHeadlines);
+        var restart = links[links.length - 1].offsetWidth;
+        console.log(left);
+        console.log("restart; " + restart);
+
+        
+        if (left <= -restart) {
+            console.log("SHOULD CHANGE THE ORDER");
+            left += links[links.length - 1].offsetWidth;
+            container.insertBefore(links[links.length - 1], links[0]);
+        }
+        container.style.right = left + "px"; 
+    }
+
+    moveHeadlines();
+
+    
+    for (var i = 0; i < links.length; i++) {
+        links[i].addEventListener("mouseenter", function (evt) {
+            console.log(evt.target + "   is the element");
+            cancelAnimationFrame(myReqId);
+        });
+        links[i].addEventListener("mouseleave", function (evt) {
+            console.log(evt.target + "   was the element");
+            moveHeadlines();
+        });
+    }
+})();
