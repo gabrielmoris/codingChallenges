be2cff79a6dac961ed5047d77c6d3caf258f5f70
Gabriel C Moris <gabrieltrompeta@gmail.com>
2021-11-12 14:54:56 +0100

    added dots in kittycarrousel and js functionalities

--- week2/carrousel/script.js
+++ week2/carrousel/script.js
@@ -1,25 +1,48 @@
 (function () {
     var kitties = document.querySelectorAll("#kitties img");
+    var dots = document.querySelectorAll(".dot");
     var currentKitty = 0;
+    var isTransitioning = false;
+    var timer;
 
-    function moveKitties() {
+    for (i = 0; i < dots.length; i++) {
+        dots[i].addEventListener("click", function (e) {
+            var dotIndex = e.target.id.slice(3);
+            if (isTransitioning) {
+                return;
+            }
+            clearTimeout(timer); //to clear the queue
+            moveKitties(dotIndex); //to tell which kitty i call when i start
+        });
+    }
+
+    function moveKitties(dotIdx) {
+        dots[currentKitty].classList.remove("on");
         kitties[currentKitty].classList.remove("onscreen");
         kitties[currentKitty].classList.add("exit-left");
 
         currentKitty++;
+
+        if (dotIdx != undefined) {
+            currentKitty = dotIdx;
+        }
+
         if (currentKitty >= kitties.length) {
             currentKitty = 0;
         }
         kitties[currentKitty].classList.add("onscreen");
+        dots[currentKitty].classList.add("on");
+        isTransitioning = true;
 
-        setTimeout(moveKitties, 4000); //recall the function each 4 seconds
+        timer = setTimeout(moveKitties, 8000); //recall the function each 4 seconds
     }
 
-    setTimeout(moveKitties, 1000);
+    timer = setTimeout(moveKitties, 1000);
     document.addEventListener("transitionend", function (e) {
         if (e.target.className == "exit-left") {
-            console.log("it works " + e.target.className);
+            // console.log("it works " + e.target.className);
             e.target.classList.remove("exit-left");
+            isTransitioning = false;
         }
     });
 })();
