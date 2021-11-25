1e45fe8fdcdbfa78793f42baf9320690dad2416b
Gabriel C Moris <gabrieltrompeta@gmail.com>
2021-11-25 16:45:37 +0100

    improved the function of infoUrl

--- week4/thursday/index.js
+++ week4/thursday/index.js
@@ -1,7 +1,9 @@
+let urlInput = process.argv[2];
 //index
 // const o = require("./index.js")
+// o.infoUrl("http://127.0.0.1:8080/test?a=100&b=200")
 const url = require("url");
-module.exports.infoUrl = function (url) {
+function infoUrl (url) {
     const myUrl = new URL(url);
     console.log("The protocol is: ", myUrl.protocol);
     console.log("The host is: ", myUrl.host);
@@ -9,10 +11,13 @@ module.exports.infoUrl = function (url) {
     console.log("The port is: ", myUrl.port);
     console.log("The pathname is: ", myUrl.pathname);
     console.log("The query is: ", myUrl.search);
-    console.log("The value of the a parameter is: ", myUrl.searchParams[0]);
-    console.log("The value of the b parameter is: ", myUrl.searchParams[1]);
-};
+    // console.log("The value of the a parameter is: ", myUrl.searchParams);
+    myUrl.searchParams.forEach((value ,key) => {
 
+      console.log("The value of the "+ key + " parameter is: " + value) ;
+    });
+};
+infoUrl(urlInput);
 // console.log("url: ",url);
 // console.log("url.parse(): ", url.parse("https://www.someWebsite.com/someInformation?scroll=infinite"))
 // const myUrl = new URL(
