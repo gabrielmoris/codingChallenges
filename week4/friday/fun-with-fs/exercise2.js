281d2f4e13c669c18dec48b72092e838c074a9c0
Gabriel C Moris <gabrieltrompeta@gmail.com>
2021-11-26 15:48:55 +0100

    added exercise 2 working

--- week4/friday/fun-with-fs/exercise2.js
+++ week4/friday/fun-with-fs/exercise2.js
@@ -1,1 +1,30 @@
 const fs = require("fs");
+
+function mapSizes(directoryPath) {
+    let mapObject = {};
+    let contentDirs = fs.readdirSync(directoryPath, { withFileTypes: true });
+    // console.log(contentDirs);
+    for (let i = 0; i < contentDirs.length; i++) {
+        if (contentDirs[i].isFile()) {
+            let siceFile = fs.statSync(
+                directoryPath + "/" + contentDirs[i].name
+            ).size;
+            mapObject[contentDirs[i].name] = siceFile;
+        } else {
+            let directoryPath2 = directoryPath + "/" + contentDirs[i].name;
+            let nestedMapObject = mapSizes(directoryPath2);
+            mapObject[contentDirs[i].name] = nestedMapObject;
+        }
+    }
+    return mapObject;
+}
+
+// console.log(mapSizes(`${__dirname}/files`));
+
+const path = __dirname + "/pathObject.json";
+try {
+    const stringedObj = JSON.stringify(mapSizes(`${__dirname}/files`), null, 8);
+    fs.writeFileSync(path, stringedObj);
+} catch (e) {
+    console.log("Error in making string: ", e); //logs 'ReferenceError: asdfasfasf is not defined'
+}
