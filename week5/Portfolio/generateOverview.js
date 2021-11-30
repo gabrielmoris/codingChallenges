526c0528656ddc54553f807f5e1339ac1a58c1d4
Gabriel C Moris <gabrieltrompeta@gmail.com>
2021-11-30 15:11:57 +0100

    clean js files of coments

--- week5/Portfolio/generateOverview.js
+++ week5/Portfolio/generateOverview.js
@@ -1,11 +1,6 @@
 const fs = require("fs");
 
 module.exports.generateOverviewHtml = function () {
-    //it should use readdir, (recomended syncronized method)
-    //read the content of the projects directory
-    //readdir returns an array of strings that contain the names of the content in the projects directory
-    //we loop over that content, compose html that generates an a tag
-    //the dfunction at the end returns that string of html
     const projects = fs.readdirSync(__dirname + "/projects", {
         withFileTypes: true,
     });
@@ -14,11 +9,9 @@ module.exports.generateOverviewHtml = function () {
     for (let i = 0; i < projects.length; i++) {
         let filePath = projects[i].name;
         let eachProject = projects[i].name;
-        //  console.log(filePath)//this is all the paths
-
         htmlToInsert += `<a href='${filePath}'>${eachProject}</a><br>`;
     }
+    
     return htmlToInsert;
 };
 
-// console.log(generateOverviewHtml());
--- week5/Portfolio/index.js
+++ week5/Portfolio/index.js
@@ -17,13 +17,13 @@ const contentType = {
 http.createServer((req, res) => {
     req.on("error", (err) => console.log("Error on request: ", err));
     res.on("error", (err) => console.log("Error on response: ", err));
-    //i only allow GET request
+
     if (req.method != "GET") {
         console.log("NOT a GET request. That's not good!!");
         req.statusCode = 405; //method not allowed
         return res.end();
     }
-    //we normalize so that we are capable to understand if there is any traversal attack attempts
+
     const filePath = path.normalize(__dirname + "\\projects" + req.url);
     // protection
     if (!filePath.startsWith(`${__dirname}\\projects\\`)) {
@@ -31,28 +31,23 @@ http.createServer((req, res) => {
         req.statusCode = 403; //forbidden
         return res.end();
     }
-    //////////////////////////////////////////////////////////////////////////////
+
     if (req.url === "/") {
-        console.log("here i will put the a tags!!!!!");
         req.statusCode = 200;
         res.setHeader("Content-Type", "text/html");
         return res.end(generateOverviewHtml());
     } else {
-        //////////////////////////////////////////////////////////////////////////////
         //sending over the files that the user requested
-        //#1
+
         fs.stat(filePath, (err, stats) => {
             if (err) {
                 console.log("FILE doesn't exist");
-                // console.log(filePath);
                 res.statusCode = 404; //not found
                 return res.end();
             }
-            //if we get here that means the user requested something that exists
 
             if (stats.isDirectory()) {
                 if (req.url.endsWith("/")) {
-                    //if whathever the user is requesting is a directory the user should be served the index.html
                     res.setHeader("Content-Type", "text/html");
                     const readstreamIndexHtml = fs.createReadStream(
                         filePath + "index.html"
@@ -65,31 +60,16 @@ http.createServer((req, res) => {
                         return res.end();
                     });
                 } else {
-                    //redirect the user to the same req.url with the slash
-                    //check back with the http request listener
-                    // exercise from yesterday
-                    // set header status code and end response
-                    // console.log("the user requests this url" + req.url);
                     res.setHeader("Location", req.url + "/");
                     res.statusCode = 302;
                     res.end();
                 }
             } else {
-                // console.log(
-                //     "user wants a file!!!!",
-                //     contentType[path.extname(filePath)]
-                // );
-                //if the user is requesting a file. that means we should send the file
-                //send that file:
-                //#1 send the correct header
-                // console.log("File extension", path.extname(filePath));
                 res.setHeader(
                     "Content-Type",
                     contentType[path.extname(filePath)]
                 );
-                //#2 create a readstream to that file
                 const readstreamFile = fs.createReadStream(filePath);
-                //#3 pipe the readstream to the response object
                 readstreamFile.pipe(res);
 
                 readstreamFile.on("error", (err) => {
@@ -100,4 +80,4 @@ http.createServer((req, res) => {
             }
         });
     }
-}).listen(8080, () => console.log("Portfolio server is listening to you!"));
+}).listen(8080, () => console.log("Portfolio server is listening to you!👂👂"));
