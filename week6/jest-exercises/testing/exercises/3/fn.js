8f5a7984805c4ebc39e434554bcedb6b1d936739
Gabriel C Moris <gabrieltrompeta@gmail.com>
2021-12-08 18:03:34 +0100

    added 3 exercise of testiing with jest

--- week6/jest-exercises/testing/exercises/3/fn.js
+++ week6/jest-exercises/testing/exercises/3/fn.js
@@ -1,2 +1,23 @@
-module.exports = function fn() {
+module.exports = function fn(value) {
+    if (typeof value === "string") {
+       
+        return reverseString(value);
+    }
+    if (typeof value === "number") {
+        return null;
+    }
+    if (Array.isArray(value)) {
+        let newArray = [];
+        for (i = 0; i < value.length; i++) {
+           newArray.push(fn(value[i]))
+        }
+        return newArray
+    }
 };
+function reverseString(string) {
+     let newString = "";
+     for (var i = string.length - 1; i >= 0; i--) {
+         newString += string[i];
+     }
+     return newString;
+ }
