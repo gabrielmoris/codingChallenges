2e97e07058eed4b7a8ad2a4be53873c22f4d7840
Gabriel C Moris <gabrieltrompeta@gmail.com>
2021-12-08 16:15:33 +0100

    Added Exercise 1

--- week6/jest-exercises/testing/exercises/1/countries.test.js
+++ week6/jest-exercises/testing/exercises/1/countries.test.js
@@ -1,7 +1,17 @@
-const countries = require('./countries');
-
+const { find } = require("./countries");
 
 test("When find is passed an empty string, it returns an empty array", () => {
-    const result = find(" ") 
-    expect(result).toBe([]);
+    expect(find("")).toEqual([]);
+});
+
+test("The array that it returns contains no more than four matches", () => {
+    expect(find("a").length).toBe(4);
+});
+
+test("The search is case insensitive", () => {
+    expect(find("AlBaNiA")).toEqual(["Albania"]);
+});
+
+test("If there are no matching countries, an empty array is returned", () => {
+    expect(find("Spanien").length).toBe(0);
 });
--- week6/jest-exercises/testing/exercises/2/albums.test.js
+++ week6/jest-exercises/testing/exercises/2/albums.test.js
@@ -4,6 +4,11 @@ const spotify = require("./spotify");
 jest.mock("./spotify");
 
 test("album names are in alphabetical order", () => {
+
+    spotify.search.mockResolvedValue(
+        
+    )
+
     return getAlbumNames("meat loaf").then((albumNames) => {
         expect(albumNames).toEqual(albumNames.slice().sort());
     });
