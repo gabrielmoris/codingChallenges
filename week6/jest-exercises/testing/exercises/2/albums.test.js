2dd474bfce1e76e3b04d565dcaf4c481ad4b4174
Gabriel C Moris <gabrieltrompeta@gmail.com>
2021-12-08 16:48:40 +0100

    added exercise 2 of testing with jest

--- week6/jest-exercises/testing/exercises/2/albums.test.js
+++ week6/jest-exercises/testing/exercises/2/albums.test.js
@@ -4,12 +4,14 @@ const spotify = require("./spotify");
 jest.mock("./spotify");
 
 test("album names are in alphabetical order", () => {
-
-    spotify.search.mockResolvedValue(
-        
-    )
+    spotify.search.mockResolvedValue({
+        albums: {
+            items: [{ name: "a" }, { name: "d" }, { name: "c" }],
+        },
+    });
 
     return getAlbumNames("meat loaf").then((albumNames) => {
+        console.log(albumNames.slice().sort());
         expect(albumNames).toEqual(albumNames.slice().sort());
     });
 });
